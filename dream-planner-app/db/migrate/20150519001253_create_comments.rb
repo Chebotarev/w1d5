c0df24efe8e36bab6e30d7fe40b50f331bb45c77
Aleksey Chebotarev Sebastian Peterlin <student@appacademy.io>
2015-05-18 17:17:25 -0700

    Add comments table

---
+++ dream-planner-app/db/migrate/20150519001253_create_comments.rb
@@ -0,0 +1,18 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.integer :author_id, null: false
+      t.integer :commentable_id, null: false
+      t.integer :commentable_type, null: false
+      t.text :body, null:false
+
+
+      t.timestamps null: false
+    end
+
+    add_index :comments, :author_id
+    add_index :comments, :commentable_id
+    add_index :comments, :commentable_type
+
+  end
+end
